d8bcdbeb5cacdd077b310efa1978e8e36e83473e
silencewwt <silencewwt@gmail.com>
2014-08-17 13:18:23 +0800

    增加后台显示

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from blog.models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Blog)
+admin.site.register(Photo)
+admin.site.register(Comment)
+admin.site.register(BlogImg)
